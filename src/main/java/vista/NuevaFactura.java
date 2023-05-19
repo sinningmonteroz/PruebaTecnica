b6ca7a8188f6418c4f77bb55a856cf1fb361e379
Ronaldo Sinning <61475248+sinningmonteroz@users.noreply.github.com>
2023-05-19 15:49:21 -0500

    EnvioParcial

--- src/main/java/vista/NuevaFactura.java
+++ src/main/java/vista/NuevaFactura.java
@@ -632,6 +632,9 @@ public class NuevaFactura extends javax.swing.JFrame {
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
+        if(numFactura.getText()=="" || IDPersona.getText()==""){
+            
+        }
         CrearFactura();
         CargarTabla1();
     }//GEN-LAST:event_jButton3ActionPerformed
